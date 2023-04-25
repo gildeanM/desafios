a0a6f95b276d114f35fa8093d900541f6875ce8b
gildeanM <113740969+gildeanM@users.noreply.github.com>
2023-04-24 21:43:26 -0300

    Add files via upload

--- desafios/desafio01/script.js
+++ desafios/desafio01/script.js
@@ -1,10 +1,22 @@
 var estoque = [];
+var emEstoque = false;
+var id = 0;
+
 
 function setEstoque(){
     var produto = document.getElementById("produto").value;
-    var quantidade = document.getElementById("quantidadeProduto").value;
-    var emEstoque = false;
-    var id = -1;
+    var quantidade = parseInt(document.getElementById("quantidadeProduto").value);
+   
+    if(estoque.length >= 1){
+        for(; id < estoque.length; id++){
+            
+        emEstoque = estoque[id].produto == produto;
+        if(emEstoque){
+            break;
+        }
+
+        }
+    }
 
     if(estoque.length == 0){
         estoque.push({
@@ -12,21 +24,7 @@ function setEstoque(){
             produto: produto,
             quantidade: quantidade
         })
-    }
-
-    // for(; id < estoque.length-1; id++){
-        
-    //    emEstoque = estoque[id+1].produto == produto;
-    //    if(emEstoque){
-    //     break;
-    //    }
-
-    // }
-
-    if(estoque.length == 0){
-
-    }else
-    if(!emEstoque && estoque.length > 1){
+    }else if(!(emEstoque && estoque.length >= 1)){
         estoque.push({
             id: estoque.length,
             produto: produto,
@@ -34,7 +32,45 @@ function setEstoque(){
         })
     }
 
-    console.log(estoque);
+    var save = estoque;
 
+    return save;
 }
 
+console.log(save)
+
+function setVenda(){
+    var produtoVendido = document.getElementById("produtoVendido").value;
+    var quantidadeVendida = parseInt(document.getElementById("quantidadeVendida").value);
+
+    for(; id < estoque.length; id++){
+
+        if(estoque[id].produto == produtoVendido){
+            
+            
+            estoque[id].quantidade = estoque[id].quantidade - quantidadeVendida;
+            break;
+        }
+
+    }
+
+    document.getElementById("newId").innerHTML = `ID ${estoque[id].id}`;
+    document.getElementById("newProduto").innerHTML = `PRODUTO ${estoque[id].produto}`;
+    document.getElementById("newQuantidade").innerHTML = `QUANTIDADE ${estoque[id].quantidade}`;
+}
+
+
+// window.onload = function pageEstoque(){
+   
+    
+//     if(estoque.length > 0){
+        
+//         console.log(estoque)
+
+//     }else{
+
+//         document.getElementById("estoquePage").innerHTML = "abcd";
+
+//     }
+
+// }
